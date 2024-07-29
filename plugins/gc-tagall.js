92424f955ef46e7514c3f9882d4b383215a27911
ZloiAdmiN <96934475+Whats-bot@users.noreply.github.com>
2024-07-29 13:29:18 +0300

    Update

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -17,7 +17,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* ZLOI-BOT\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* *Вас потревожил ZLOI-BOT*\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
